97ede3e981fa3dd511b3773e4e0f77097fc6e6ac
Jean-Charles <jeancharles.duffau@gmail.com>
2022-01-04 15:03:27 +0100

    fin de todoliste

--- ToDoListe_react/src/Components/Form.js
+++ ToDoListe_react/src/Components/Form.js
@@ -1,23 +1,70 @@
 import React, { useState } from 'react'
 import Item from './Item';
-
+import { v4 as uuidv4 } from 'uuid';
 
 export default function Form() {
+
+
+
+    const [dataArr, setDataArr] = useState([
+        { txt: 'Promener le chien', id: uuidv4() },
+        { txt: 'Sport', id: uuidv4() },
+        { txt: 'Coder avec react', id: uuidv4() }
+    ])
+
+    const [stateInput, setStateInput] = useState('')
+
+    const deleteElement = (id) => {
+        //console.log(id);
+        const filteredState = dataArr.filter(item => {
+            return item.id !== id
+        })
+        setDataArr(filteredState)
+    }
+
+    // remplacer par composant controllé, mise à jour du state via onChange
+    // const linkedInput = (e) => {
+    //     //console.log(e);
+    //     setStateInput(e)
+    // }
+
+    const addTodo = (e) => {
+        e.preventDefault()
+        const newArr = [...dataArr]
+        const newTodo = {}
+
+        newTodo.txt = stateInput
+        newTodo.id = uuidv4()
+
+        newArr.push(newTodo)
+        setDataArr(newArr)
+        setStateInput('')
+    }
+
     return (
         <div className='m-auto px-4 col-12 col-sm-10 col-lg-6'>
-            <form className='mb-3'>
+            
+            <form onSubmit={(e)=>addTodo(e)} className='mb-3'>
                 <label htmlFor="todo" className="form-label mt-3">
                     Chose à faire</label>
-                <input type="text" className="form-control" id="todo" />
+                
+                <input
+                    onChange={e => setStateInput(e.target.value)}
+                    value={stateInput}
+                    type="text"
+                    className="form-control"
+                    id="todo" />
+
                 <button className="mt-2 btn btn-primary d-block">Envoyez</button>
             </form>
 
             <h2>Liste des chose à faire</h2>
             <ul className="list-group">
-                <Item />
-                <Item />
-                <Item />
-                <Item />
+                {dataArr.map((item) => {
+                    return (
+                        <Item txt={item.txt} key={item.id} delFunc={deleteElement} id={item.id} />
+                    )
+                })}
             </ul>
         </div>
     )
--- ToDoListe_react/src/Components/Item.js
+++ ToDoListe_react/src/Components/Item.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-export default function Item() {
+export default function Item(props) {
     return (
         <div>
             <li className="border justify-content-between d-flex align-items-center p-2 m-2">
-                <div className="p-3">Txt</div>
-                <button className="btn btn-danger p-2 h-50">
+                <div className="p-3">{props.txt}</div>
+                <button className="btn btn-danger p-2 h-50" onClick={()=>props.delFunc(props.id)}>
+                    
                     Supprimer
                 </button>
             </li>
