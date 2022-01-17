0b860b2954c459507434a1d6df107e4cc327ec55
Jean-Charles <jeancharles.duffau@gmail.com>
2022-01-17 14:41:12 +0100

    Fin de la partie création composants

--- react-creationComposant/src/App.js
+++ react-creationComposant/src/App.js
@@ -2,16 +2,17 @@ import { useState } from 'react';
 import './App.css';
 import Modal from './Components/Modal/Modal';
 import Accord from './Components/Accord/Accord'
-
+import Navbar from './Components/Navbar/Navbar';
 
 
 function App() {
 
     return (
         <div className="App">
+            <Navbar/>
             {/* <Modal /> */}
 
-            <Accord/>
+            {/* <Accord/> */}
         </div>
     );
 }
